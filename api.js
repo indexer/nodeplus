cfe01e6783a7f7f655306821772b27c99ff25d42
Indexer <yemonkyaw007@gmail.com>
2013-06-17 07:13:09 +0630

    connected with Mongo DB

--- api.js
+++ api.js
@@ -2,6 +2,11 @@ var express = require('express'),
     friends = require('./routes/friend');
 var app = express();
 
+app.configure(function()
+        {
+            app.use(express.logger('dev'));
+            app.use(express.bodyParser());
+        });
 app.get('/friends',friends.findAll);
 app.listen(3000);
 console.log('Listening on port 3000');
--- routes/friend.js
+++ routes/friend.js
@@ -1,4 +1,61 @@
+var mongo = require('mongodb');
+
+var Server= mongo.Server,
+    Db = mongo.Db,
+    BSON=mongo.BSONPure;
+
+var server = new Server('localhost',27017,{auto_reconnect:true});
+db = new Db('friend',server);
+
+db.open(function(err,db)
+        {
+            if(!err)
+             {
+                 console.log("Connected to 'friendsdb' database");
+                 db.collection('friends',{strict:true},function(err,collection)
+                     {
+                         if(err)
+                          {
+                              console.log("The 'friends' collection does not exit");
+                            populateDB();                          }
+                     });
+             }
+        
+        });
+
 exports.findAll = function(req,res)
 {
-    res.send([{name:'Naing Ye Min'},{name:'Bone Pyae Sone'},{name:'Thiha Tun'}]);
-}
+    db.collection('friends',function(err,collection)
+    {
+        collection.find().toArray(function(err,items)
+        {
+            res.send(items);
+        });
+    });
+
+};
+
+var populateDB=function()
+{
+    var friends = [
+    {
+        name: "Naing Ye Min",
+        age:'30',
+        country:'Myanmar'
+    },
+    {
+        name:"Bone Pyae Sone",
+        age:"20",
+        country:'Myanmar'
+    },
+    {
+        name:"Thiha Tun",
+        age:"24",
+        country:"Myanmar"
+    }
+        ];
+
+        db.collection('friends',function(err,collection){
+        collection.insert(friends,{safe:true},function(err,result){}); 
+        });
+};
